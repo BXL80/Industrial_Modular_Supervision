903ce19619c6876f4c8a023ce2cca9a89d3fe6d9
BXL80 <bixente.lecadieu@etu.unilasalle.fr>
2024-12-04 19:04:27 +0100

    Toutes heures OK

--- backend/BDD/schema.sql
+++ backend/BDD/schema.sql
@@ -62,5 +62,5 @@ VALUES (3, 'IT');
 INSERT IGNORE INTO Automates 
 (nom_machine, nom_automate, ip_automate, port_connexion, bibliotheque, numero_registre, taille_registre, type_donnees, etat_bit, date_heure_paris)
 VALUES
-('AU', 'Zone4', '172.16.1.24', 502, 'Modbus-Serial', 514, 1, 'readCoils', 1, CONVERT_TZ(NOW(), 'UTC', 'Europe/Paris')),
-('Temperature', 'Zone4', '172.16.1.24', 502, 'Modbus-Serial', 503, 1, 'readHoldingRegisters', 0, CONVERT_TZ(NOW(), 'UTC', 'Europe/Paris'));
+('AU', 'Zone4', '172.16.1.24', 502, 'Modbus-Serial', 514, 1, 'readCoils', 1, NOW()),
+('Temperature', 'Zone4', '172.16.1.24', 502, 'Modbus-Serial', 503, 1, 'readHoldingRegisters', 0, NOW());
